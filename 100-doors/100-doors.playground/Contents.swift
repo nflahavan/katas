87a0148cd8ac2244df121691a9d1ee579248315a
Nik Flahavan <nflahavan@gmail.com>
2020-07-01 08:57:09 -0700

    100-doors: Added what I think should be a sufficient number of tests.

--- 100-doors/100-doors.playground/Contents.swift
+++ 100-doors/100-doors.playground/Contents.swift
@@ -13,22 +13,72 @@ import XCTest
 
 class Tests: XCTestCase {
   func testDoorOneIsOpen() {
-    let actual = doorStatesAfter100Passes().first
+    let state = doorStatesAfter100Passes()
+    guard !state.isEmpty else { XCTFail(); return }
+    
+    let actual = state[0]
     let expected = DoorState.open
+    
     XCTAssertEqual(actual, expected)
   }
   
   func testDoorTwoIsClosed() {
+    let state = doorStatesAfter100Passes()
+    guard state.count > 1 else { XCTFail(); return }
+    
     let actual = doorStatesAfter100Passes()[1]
     let expected = DoorState.closed
+    
+    XCTAssertEqual(actual, expected)
+  }
+  
+  func testDoorThreeIsClosed() {
+    let state = doorStatesAfter100Passes()
+    guard state.count > 2 else { XCTFail(); return }
+    
+    let actual = state[2]
+    let expected = DoorState.closed
+    
     XCTAssertEqual(actual, expected)
   }
   
   func testDoorFourIsOpen() {
-    let actual = doorStatesAfter100Passes()[3]
+    let state = doorStatesAfter100Passes()
+    guard state.count > 3 else { XCTFail(); return }
+    
+    let actual = state[3]
     let expected = DoorState.open
+    
+    XCTAssertEqual(actual, expected)
+  }
+  
+  func testDoor17IsClosed() {
+    let state = doorStatesAfter100Passes()
+    guard state.count > 16 else { XCTFail(); return }
+    
+    let actual = state[16]
+    let expected = DoorState.closed
+    
+    XCTAssertEqual(actual, expected)
+  }
+  
+  func testDoor100IsOpen() {
+    let state = doorStatesAfter100Passes()
+    guard state.count > 99 else { XCTFail(); return }
+    
+    let actual = state[99]
+    let expected = DoorState.open
+    
+    XCTAssertEqual(actual, expected)
+  }
+  
+  func testDoor100IsLast() {
+    let actual = doorStatesAfter100Passes().count
+    let expected = 100
+    
     XCTAssertEqual(actual, expected)
   }
 }
+  
 
 Tests.defaultTestSuite.run()
