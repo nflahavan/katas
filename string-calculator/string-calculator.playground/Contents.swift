07487a4f1f704681b3a88614e576ebe6f1d7f0e3
Nik Flahavan <nflahavan@gmail.com>
2020-06-30 19:01:57 -0700

    string-calculator: first test fails

--- string-calculator/string-calculator.playground/Contents.swift
+++ string-calculator/string-calculator.playground/Contents.swift
@@ -1,7 +1,15 @@
+func addNumbers(_ numbers: String) -> Int {
+  return Int.min
+}
+
 import XCTest
 
 class Tests: XCTestCase {
-
+  func testEmptyStringIsZero() {
+    let actual = addNumbers("")
+    let expected = 0
+    XCTAssertEqual(actual, expected)
+  }
 }
 
 Tests.defaultTestSuite.run()
